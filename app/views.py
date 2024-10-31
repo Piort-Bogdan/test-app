027da31814cf0339d85bb781a0cca7da47855c20
petrbogdan <piort.bogdan@gmail.com>
2024-10-31 21:54:40 +0200

    refactoring: added filtering and pagination

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
+from rest_framework.filters import SearchFilter
+from rest_framework_json_api import filters, django_filters
+
 from app import models
 from app.serializers import TransactionSerializer, WalletSerializer
 from rest_framework import viewsets
@@ -6,8 +9,22 @@ from rest_framework import viewsets
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = models.Transaction.objects.all()
     serializer_class = TransactionSerializer
+    filter_backends = (filters.QueryParameterValidationFilter, filters.OrderingFilter,
+                       django_filters.DjangoFilterBackend, SearchFilter)
+    filterset_fields = {
+        'txid': ('icontains', 'iexact', 'contains'),
+        'amount': ('gte', 'lte', 'exact'),
+    }
+    search_fields = ('txid', 'amount',)
 
 
 class WalletViewSet(viewsets.ModelViewSet):
     queryset = models.Wallet.objects.all()
     serializer_class = WalletSerializer
+    filter_backends = (filters.QueryParameterValidationFilter, filters.OrderingFilter,
+                       django_filters.DjangoFilterBackend, SearchFilter)
+    filterset_fields = {
+        'label': ('icontains', 'iexact', 'contains'),
+        'balance': ('gte', 'lte', 'exact'),
+    }
+    search_fields = ('label', 'balance',)
