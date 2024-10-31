035a1a9c018468fe75cf77659eee155b3de20acc
petrbogdan <piort.bogdan@gmail.com>
2024-10-31 22:59:48 +0200

    feat: balance control logic moved to serializers.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from django.core.validators import MinValueValidator
 from django.db import models, transaction
-from django.utils.translation.trans_real import translation
+from django.db.models import Sum
 
 
 class Transaction(models.Model):
@@ -8,21 +8,9 @@ class Transaction(models.Model):
     txid = models.CharField(max_length=255, unique=True)
     amount = models.DecimalField(max_digits=36, decimal_places=18)
 
-    def save(self, *args, **kwargs):
-        with transaction.atomic():
-
-            super(Transaction, self).save(*args, **kwargs)
-            new_balance = self.amount + self.wallet.balance
-            if new_balance < 0:
-                raise ValueError('Not enough balance')
-
-            self.wallet.balance = new_balance
-            self.wallet.save()
-
-
-
     def __str__(self):
         return self.txid
+
 
 class Wallet(models.Model):
     label = models.CharField(max_length=255)
--- app/serializers.py
+++ app/serializers.py
@@ -1,3 +1,5 @@
+from django.db import transaction
+from django.db.models import Sum
 from rest_framework_json_api import serializers
 
 from app import models
@@ -7,6 +9,24 @@ class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Transaction
         fields = ['wallet', 'txid', 'amount']
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            transaction_instance = super().create(validated_data)
+
+            new_balance = transaction_instance.wallet.transactions.aggregate(amount_total=Sum('amount'))[
+                              'amount_total'] or 0
+
+            # check for sufficient balance
+            if new_balance < 0:
+                raise serializers.ValidationError("Not enough balance")
+
+            # Update wallet balance
+            transaction_instance.wallet.balance = new_balance
+            transaction_instance.wallet.save(update_fields=['balance'])
+
+            return transaction_instance
+
 
 class WalletSerializer(serializers.ModelSerializer):
     class Meta:
