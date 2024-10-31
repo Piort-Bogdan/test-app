76d43f014d93e30ec8e5ae3e7861002969d6881e
petrbogdan <piort.bogdan@gmail.com>
2024-10-31 23:00:56 +0200

    feat: started working on tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from app import models
+
+
+class TestTransaction(TestCase):
+    def setUp(self):
+
+        self.wallet = models.Wallet.objects.create(label='test')
+        self.transaction = models.Transaction.objects.create(wallet=self.wallet, txid='test', amount=10)
+
+    def test_not_enough_balance(self):
+        pass
