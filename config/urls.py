8ba6990d1d18357bbc91c4e1d5fc8e289f4094be
petrbogdan <piort.bogdan@gmail.com>
2024-10-31 21:54:25 +0200

    refactoring: added api versioning

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ from app.urls import router
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include(router.urls)),
+    path('api/v1/', include(router.urls)),
 ]
