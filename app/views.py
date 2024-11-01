b1ac9cd2b85f71d22ef7b4c9c6988402d1a90a76
petrbogdan <piort.bogdan@gmail.com>
2024-11-01 23:09:56 +0200

    refactor: canceled permission_classes, added example response for 201, 200, 400 response statuses

--- app/views.py
+++ app/views.py
@@ -1,43 +1,20 @@
-from django.http import JsonResponse
-from drf_spectacular.utils import extend_schema, extend_schema_view
-from rest_framework.authtoken.models import Token
+from drf_spectacular.utils import extend_schema, OpenApiResponse
 from rest_framework.filters import SearchFilter
-from rest_framework.views import APIView
 from rest_framework_json_api import filters, django_filters
 
 from app import models
-from app.serializers import TransactionSerializer, WalletSerializer
-from rest_framework import viewsets, permissions, status
-
-
-@extend_schema(
-    tags=['Auth'],
-    responses={200: {'token': 'string'}},
-)
-class CustomAuthToken(APIView):
-    permission_classes = [permissions.AllowAny]
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-
-        return JsonResponse({
-            'token': token.key,
-        }, status=status.HTTP_200_OK)
+from app.serializers import TransactionSerializer, WalletSerializer, ErrorResponseSerializer
+from rest_framework import viewsets, status
 
 
 @extend_schema(
     tags=['Transactions'],
-    request=TransactionSerializer,
-    responses={201: TransactionSerializer},
-    methods=['GET', 'POST', 'DELETE'],
-    )
+    request=TransactionSerializer
+)
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = models.Transaction.objects.all()
     serializer_class = TransactionSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # commented to reduce features testing complexity
     filter_backends = (filters.QueryParameterValidationFilter, filters.OrderingFilter,
                        django_filters.DjangoFilterBackend, SearchFilter)
     filterset_fields = {
@@ -46,34 +23,45 @@ class TransactionViewSet(viewsets.ModelViewSet):
     }
     search_fields = ('txid', 'amount',)
 
+    @extend_schema(
+        responses={
+            status.HTTP_201_CREATED: TransactionSerializer,
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(ErrorResponseSerializer, description="Not enough balance"),
+        },
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @extend_schema(
+        responses={status.HTTP_200_OK: TransactionSerializer},
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
     def destroy(self, request, *args, **kwargs):
         # allows only staff users to delete transactions
-        if not request.user.is_staff:
-            return JsonResponse({'detail': 'Permission denied.'}, status=403)
+        # if not request.user.is_staff:
+        #     return JsonResponse({'detail': 'Permission denied.'}, status=403)  # commented to reduce features testing complexity
 
         return super().destroy(request, *args, **kwargs)
 
-    @extend_schema(
-        exclude=True)
+    @extend_schema(exclude=True)
     def update(self, request, *args, **kwargs):
         super().update(request, *args, **kwargs)
 
-    @extend_schema(
-        exclude=True)
+    @extend_schema(exclude=True)
     def partial_update(self, request, *args, **kwargs):
         super().partial_update(request, *args, **kwargs)
 
 
 @extend_schema(
     tags=['Wallets'],
-    request=TransactionSerializer,
-    responses={201: TransactionSerializer},
-    methods=['GET', 'POST', 'DELETE'],
+    request=WalletSerializer,
     )
 class WalletViewSet(viewsets.ModelViewSet):
     queryset = models.Wallet.objects.all()
     serializer_class = WalletSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # commented to reduce features testing complexity
     filter_backends = (filters.QueryParameterValidationFilter, filters.OrderingFilter,
                        django_filters.DjangoFilterBackend, SearchFilter)
     filterset_fields = {
@@ -82,19 +70,29 @@ class WalletViewSet(viewsets.ModelViewSet):
     }
     search_fields = ('label', 'balance',)
 
+    @extend_schema(
+        responses={status.HTTP_201_CREATED: WalletSerializer},
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @extend_schema(
+        responses={status.HTTP_200_OK: WalletSerializer},
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
     def destroy(self, request, *args, **kwargs):
         # allows only staff users to delete wallets
-        if not request.user.is_staff:
-            return JsonResponse({'detail': 'Permission denied.'}, status=403)
+        # if not request.user.is_staff:
+        #     return JsonResponse({'detail': 'Permission denied.'}, status=403)  # commented to reduce features testing complexity
 
         return super().destroy(request, *args, **kwargs)
 
-    @extend_schema(
-    exclude=True)
+    @extend_schema(exclude=True)
     def update(self, request, *args, **kwargs):
         super().update(request, *args, **kwargs)
 
-    @extend_schema(
-    exclude=True)
+    @extend_schema(exclude=True)
     def partial_update(self, request, *args, **kwargs):
         super().partial_update(request, *args, **kwargs)
