c6c34b48b9eb93c0116c5dedc2fb09dbcdca05c1
petrbogdan <piort.bogdan@gmail.com>
2024-11-01 23:07:53 +0200

    refactor: rewriting test without user to reduce feature test complexity

--- app/tests.py
+++ app/tests.py
@@ -8,66 +8,93 @@ from app import models
 
 class TestTransaction(TestCase):
     def setUp(self):
-        self.test_user = User.objects.create_user(username='test', password='test')
+        # self.test_user = User.objects.create_user(username='test', password='test')
 
         self.test_wallet = models.Wallet.objects.create(label='test')
-        self.test_transaction = models.Transaction.objects.create(wallet=self.wallet, txid='test', amount=10)
 
     def test_create_wallet(self):
         client = APIClient()
-        client.login(username='test', password='test')
 
-        response = client.post('/api/v1/wallets/', {'label': 'new_wallet'}, format='json')
+        response = client.post('/api/v1/wallets/', {
+              "data": {
+                "type": "Wallet",
+                "attributes": {
+                  "label": "test_label"
+                }
+              }
+            })
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(models.Wallet.objects.count(), 2)
-        self.assertEqual(models.Wallet.objects.get(id=response.data['id']).label, 'new_wallet')
+        self.assertEqual(models.Wallet.objects.get(id=response.data['id']).label, 'test_label')
 
     def test_balance_gets_negative(self):
         client = APIClient()
-        client.login(username='test', password='test')
+        # client.login(username='test', password='test')
 
-        response = client.post('/api/v1/transactions/', {'wallet': self.test_wallet.id, 'txid': 'tx2', 'amount': -20},
-                               format='json')
+        response = client.post('/api/v1/transactions/', {
+            "data": {
+                "type": "Transaction",
+                "attributes": {
+                  "txid": "test_txid",
+                  "amount": "-10"
+                },
+                "relationships": {
+                  "wallet": {
+                    "data": {
+                      "id": self.test_wallet.id,
+                      "type": "Wallet"
+                    }
+                  }
+            }
+            }
+        })
+
+        self.test_wallet.refresh_from_db()
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn('Not enough balance', response.data['non_field_errors'])
+        self.assertEqual(response.data[0]['detail'], 'Not enough balance')
 
     def test_balance_increment_by_transaction(self):
         client = APIClient()
-        client.login(username='test', password='test')
+        # client.login(username='test', password='test')
 
-        response = client.post('/api/v1/transactions/', {'wallet': self.test_wallet.id, 'txid': 'tx2', 'amount': 20},
-                               format='json')
+        response = client.post('/api/v1/transactions/', {
+            "data": {
+                "type": "Transaction",
+                "attributes": {
+                    "txid": "test_txid",
+                    "amount": "400"
+                },
+                "relationships": {
+                    "wallet": {
+                        "data": {
+                            "id": self.test_wallet.id,
+                            "type": "Wallet"
+                        }
+                    }
+                }
+            }
+        })
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.test_wallet.refresh_from_db()
-        self.assertEqual(self.test_wallet.balance, 30)
+        self.assertEqual(self.test_wallet.balance, 400)
 
     def test_delete_wallet(self):
         client = APIClient()
-        client.login(username='test', password='test')
+        # client.login(username='test', password='test')
 
         response = client.delete(f'/api/v1/wallets/{self.test_wallet.id}/')
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(models.Wallet.objects.count(), 0)
 
-    def test_create_transaction(self):
-        client = APIClient()
-        client.login(username='test', password='test')
-
-        response = client.post('/api/v1/transactions/', {'wallet': self.test_wallet.id, 'txid': 'tx2', 'amount': 20},
-                               format='json')
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(models.Transaction.objects.count(), 2)
-        self.assertEqual(models.Transaction.objects.get(txid='tx2').amount, 20)
-
     def test_delete_transaction(self):
         client = APIClient()
-        client.login(username='test', password='test')
+        # client.login(username='test', password='test')
 
+        self.test_transaction = models.Transaction.objects.create(wallet=self.test_wallet, txid='test_txid', amount=400)
         response = client.delete(f'/api/v1/transactions/{self.test_transaction.id}/')
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
