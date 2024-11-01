f900773ff949f3e506e729edc49e996910cb28a6
petrbogdan <piort.bogdan@gmail.com>
2024-11-01 23:06:54 +0200

    feat: added ErrorSerializer, added 'id' field to serializers

--- app/serializers.py
+++ app/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import transaction
 from django.db.models import Sum
 from rest_framework_json_api import serializers
@@ -8,7 +9,7 @@ from app import models
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Transaction
-        fields = ['wallet', 'txid', 'amount']
+        fields = ['id', 'wallet', 'txid', 'amount']
 
     def create(self, validated_data):
         with transaction.atomic():
@@ -31,6 +32,25 @@ class TransactionSerializer(serializers.ModelSerializer):
 class WalletSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Wallet
-        fields = ['label', 'balance', 'transactions']
+        fields = ['id', 'label', 'balance', 'transactions']
         depth = 1
         read_only_fields = ['balance']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = models.User.objects.create_user(**validated_data)
+
+        return user
+
+
+class ErrorResponseSerializer(serializers.Serializer):
+    detail = serializers.CharField()
+
+    class Meta:
+        resource_name = "error"
