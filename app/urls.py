ec7a53edc357a380153ed1e49bf4731064e3e366
petrbogdan <piort.bogdan@gmail.com>
2024-11-01 23:08:22 +0200

    fix: removed auth

--- app/urls.py
+++ app/urls.py
@@ -6,10 +6,3 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'transactions', views.TransactionViewSet, basename='transaction')
 router.register(r'wallets', views.WalletViewSet, basename='wallet')
-
-
-urlpatterns = [
-    path('auth/', views.CustomAuthToken.as_view(), name='auth'),
-]
-
-urlpatterns.extend(router.urls)
